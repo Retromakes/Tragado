5eabc2fe20706f6fd4d9eba3e9602a393e6c75c6
Retromakes <yaumito@gmail.com>
2024-08-29 12:48:06 +0200

    eliminados los comentarios de hotspots_custom.h para captura de pantalla de telegram a ver si alguien me hace caso con un mibujo :'(

--- dev/my/ci/hotspots_custom.h
+++ dev/my/ci/hotspots_custom.h
@@ -2,82 +2,62 @@
 // Copyleft 2010-2014, 2020 by the Mojon Twins
 
 case 1:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 2:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 3:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 4:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 5:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 6:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 7:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 8:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 9:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 10:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 11:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 12:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 13:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 14:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 15:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 16:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 17:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 18:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 19:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
 case 20:
-    // polvos magicos (16+17=33)
     success = playing = 0;
     break;
