f70b7c265d8ba318eceb1cbe3b0a71a620a82185
Retromakes <yaumito@gmail.com>
2024-09-16 09:30:20 +0200

    mapa con tiles extra en el tileset pero aun no las uso y todo se ve bien

--- dev/my/ci/entering_game.h
+++ dev/my/ci/entering_game.h
@@ -16,8 +16,9 @@ timer_frames = 50;    // Asume que el juego corre a 25 cuadros por segundo
 
 // inventario inicial
 flags[30] = 0;
-
+/*
 // debug teletransporto a pantalla inicial
-n_pant = 27;
+n_pant = 28;
 gpx = 9 << 4; p_x = gpx << FIXBITS;
 gpy = 7 << 4; p_y = gpy << FIXBITS;
+*/
